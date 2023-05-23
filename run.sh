7bde361e59e08e20d3bcf87c5338533c89608a07
Šimen Ravnik <ravnik.simen@gmail.com>
2023-05-23 22:54:58 +0200

    Seperate train_mlp_serial into c and header files

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 module load CUDA/10.1.243-GCC-8.3.0
 
-nvcc train.cu -O2 -lm -o train
+train=(
+    "src/serial/train_mlp_serial.c"
+)
+
+nvcc -O2 -lm -o train train.cu -include "${train[@]}"
 srun --reservation=fri -G1 -n1 train > results/results.txt
