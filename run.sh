97b2e03b26be5bc060b04f4776e048096ed2d794
Šimen Ravnik <ravnik.simen@gmail.com>
2023-05-26 14:13:28 +0200

    Move cuda train file to ./src/cuda

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 module load CUDA
 
-train=(
-    "src/serial/train_mlp_serial.c"
-)
+# !!! THIS IS FOR ARNES !!!
+# Fix if you want to run on NSC
 
-nvcc -O2 -lm -o train.bin train.cu -include "${train[@]}"
+# SERIAL
+gcc -O2 -lm -o train_serial.bin src/serial/train.c
+srun --reservation=fri-vr --partition=gpu train_serial.bin > results/Serial.txt
 
-srun --reservation=fri-vr --partition=gpu --gpus=1 train.bin serial > results/Serial.txt
-srun --reservation=fri-vr --partition=gpu --gpus=1 train.bin cuda > results/CUDA.txt
-srun --reservation=fri-vr --partition=gpu --gpus=1 train.bin cuda_new > results/CUDA_NEW.txt
+# OPENMP
+gcc -O2 -lm -fopenmp -o train_openmp.bin src/openmp/train.c
+srun --reservation=fri-vr --partition=gpu --cpus-per-task=4 train_openmp.bin > results/OpenMP.txt
+
+# CUDA
+nvcc -O2 -lm -o train_cuda.bin src/cuda/train.cu
+srun --reservation=fri-vr --partition=gpu --gpus=1 train_cuda.bin cuda > results/CUDA.txt
+srun --reservation=fri-vr --partition=gpu --gpus=1 train_cuda.bin new > results/CUDA_NEW.txt
+
+rm train_serial.bin train_openmp.bin train_cuda.bin
---
+++ src/cuda/train.cu
@@ -0,0 +1,55 @@
+/*
+ * Search for "TODO" comments to find the parts that need improvements.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "../../parameters.h"
+#include "../../lib/read/read.h"
+#include "../../lib/matrix/matrix.h"
+#include "../../lib/helpers/helpers.h"
+#include "../../lib/matrix/cuda_matrix.h"
+#include "../../lib/models/mlp_model.h"
+#include "../../lib/time/cuda_timer.h"
+
+#include "train_mlp_cuda.h"
+#include "train_mlp_cuda_new.h"
+
+int main(int argc, char** argv) {
+
+    // Prepare dataset
+    TrainTestSplit split = prepare_dataset(FILEPATH, CLASSES);
+
+    MLP_model model;
+    
+    // Start timer
+    cudaEvent_t start, stop;
+    cuda_start_timer(&start, &stop);
+
+    // Train the model
+    if (strcmp(argv[1], "cuda") == 0) {
+        printf("CUDA...\n");
+        model = train_mlp_cuda(split.X_train, split.Y_train, HIDDEN_SIZE, ETA, BATCH_SIZE, EPOCHS);
+    } else if (strcmp(argv[1], "new") == 0) {
+        printf("CUDA_NEW...\n");
+        model = train_mlp_cuda_new(split.X_train, split.Y_train, HIDDEN_SIZE, ETA, BATCH_SIZE, EPOCHS);
+    } else {
+        printf("Invalid argument. Please use either 'serial' or 'cuda'.\n");
+        return 1;
+    }
+    
+    // Stop timer
+    printf("Time: %0.3f milliseconds \n", cuda_stop_timer(&start, &stop));
+
+    // Test the model
+    float accuracy = predict(split.X_train, split.Y_train, model);
+    printf("Accuracy: %f\n\n", accuracy);
+
+    // Free memory
+    free_split(split);
+    free_model(model);
+    
+    return 0;
+}
