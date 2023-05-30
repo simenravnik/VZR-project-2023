b524f4d7d633b2bf14900d11a8d6586d4d92c046
Šimen Ravnik <ravnik.simen@gmail.com>
2023-05-30 22:12:50 +0200

    Update parameters default value back to winequalityN.csv dataset

--- parameters.h
+++ parameters.h
@@ -1,7 +1,7 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-const char* FILEPATH = "data/mnist.csv";
+const char* FILEPATH = "data/winequalityN.csv";
 const float TRAIN_SIZE_PERCENTAGE = 0.8;    // If 1.0, we use the training set for training and testing
 const int CLASSES = 10;
 
@@ -11,6 +11,6 @@ const int EPOCHS = 10;
 const float ETA = 0.01;
 
 // CUDA
-const int CUDA_BLOCK_SIZE = 32;
+const int CUDA_BLOCK_SIZE = 256;
 
 #endif
